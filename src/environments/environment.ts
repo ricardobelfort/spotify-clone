648e14bd8f07db03c6964c396140b2025aaa0118
Ricardo Belfort <ricardo.belfort@opah.com.br>
2022-01-14 20:43:49 -0400

    Ajustes iniciais de projetos

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,22 @@ export const environment = {
   production: false
 };
 
+export const spotifyConfig = {
+  clientId: '1abea850ed9741398a21d3eb70d64a27',
+  authEndpoint: 'https://accounts.spotify.com/authorize',
+  redirectUrl: 'http://localhost:4200/login/',
+  scopes: [
+    "user-read-currently-playing",  // música tocando agora
+    "user-read-recently-played",  // ler músicas tocadas recentemente
+    "user-read-playback-state", // ler estado do player do usuário
+    "user-top-read",  // top artistas e músicas do usuário
+    "user-modify-playback-state", // alterar o player do usuário
+    "user-library-read",  // ler biblioteca dos usuários
+    "playlist-read-private",  // ler playlists privadas
+    "playlist-read-collaborative",  // ler playlists colaborativas
+  ]
+}
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
